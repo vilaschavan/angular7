d60d7620741c0b6e2fd37c448f9da2dbd6b535e4
vilas chavan <vilacha1@in.ibm.com>
2019-12-16 12:14:54 +0530

    Registration service GET and POST converted using observable

--- src/app/services/registration-details.service.ts
+++ src/app/services/registration-details.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RegistrationDetails } from '../interfaces/registration-details';
+import { map } from 'rxjs/operators'; // This is where I import map operator
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -15,12 +16,17 @@ const httpOptions = {
 
 export class RegistrationDetailsService {
   url = 'https://udemy-aee88.firebaseio.com/data.json';
+  // josnurl = 'https://udemy-aee88.firebaseio.com/data/-Lh9k2t3I__kTAmi6ATT.json';
 
   constructor(private http: HttpClient) { }
 
   /** POST: Add data to database */
   registerdetails(Registration: RegistrationDetails): Observable<RegistrationDetails> {
     return this.http.post<RegistrationDetails>(this.url, Registration, httpOptions);
+  }
+
+  getInfo(): Observable<RegistrationDetails[]> {
+    return this.http.get<RegistrationDetails[]>(this.url);
   }
 
 }
